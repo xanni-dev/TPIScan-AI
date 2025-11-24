87cc36481c23be222564c0fa5f8b48f17276dcae
xanni-dev <danielcharlesonly@gmail.com>
2025-11-24 12:31:43 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,6 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { Toaster } from 'sonner'
+import { createRoot } from "react-dom/client"
+import App from "./App.tsx"
+import "./index.css"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-    <Toaster richColors position="top-center" />
-  </React.StrictMode>
-)
+createRoot(document.getElementById("root")!).render(<App />)
